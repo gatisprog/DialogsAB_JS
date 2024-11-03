8ff6a8d591ed77546a5a587b02f8d9539a7bc6f8
gatisprog <kurts@inbox.lv>
2024-11-03 18:35:17 +0200

    Class_05

--- Class_05/scripts/script1.js
+++ Class_05/scripts/script1.js
@@ -1,7 +1,16 @@
 const cars = ["Saab", "Volvo", "BMW"];
-
 document.getElementById("array_initial").innerHTML = `${cars} masīva izmērs = ${cars.length} elementi`;
 
+// cars[10] = "Renoult"; - tā nedarām!
 var N = cars.push("WV");
-
 document.getElementById("array_push").innerHTML = `${cars} masīva izmērs = ${N} elementi`;
+
+// delete nedarām
+//delete cars [0];
+//document.getElementById("array_delete").innerHTML = `${cars} masīva izmērs = ${cars.length} elementi`;
+
+var cars_last = cars.pop();
+document.getElementById("array_pop").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi (izdzēstais elements ${cars_last})`;
+
+var cars_first = cars.shift();
+document.getElementById("array_shift").innerHTML = `${cars} masīva izmērs - ${cars.length} elementi (izdzēstais elements ${cars_first})`;

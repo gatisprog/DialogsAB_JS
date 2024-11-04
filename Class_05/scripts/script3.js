bd6d37cf4e40314b7bb76b437b1b7cd89339ef63
gatisprog <kurts@inbox.lv>
2024-11-04 18:03:38 +0200

    Class_05

---
+++ Class_05/scripts/script3.js
@@ -0,0 +1,12 @@
+N = numbers.lenghth
+var i = 0
+
+console.log(numbers);
+console.log(i < N);
+
+while (i < N) {
+    console.log(i < N);
+    console.log(i, numbers[i]);
+    i++;
+    console.log(i < N);
+}

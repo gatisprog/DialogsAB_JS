e8e02ab4498dba6b67b08f7dc1728276815d9ca4
gatisprog <kurts@inbox.lv>
2024-11-06 21:06:50 +0200

    Class_07

--- Class_07/scripts/script_function.js
+++ Class_07/scripts/script_function.js
@@ -1,12 +1,29 @@
-function funkcijas_vards_1() {}
-function funkcijas_vards_2() {return 1;}
+function funkcijas_vaards_1() {}
 
-function square(number) {
-    return number * number;
-  }
+function funkcijas_vaards_2() {
+  return 1;
+}
 
-funkcijas_vards_1();
-funkcijas_vards_2();
+function square_1(number) {
+  return number * number;
+}
 
-document.getElementById("demo").innerHTML = `Skaitļa ${5} Kvadrāts ir ${square(5)}`;
+const square_2 = function (number) {
+  return number * number;
+};
 
+const factorial = function fac(n) {
+    return n < 2 ? 1 : n * fac(n - 1);
+  };
+
+funkcijas_vaards_1();
+funkcijas_vaards_2();
+
+document.getElementById("demo").innerHTML = `Skaitļa ${5} kvadrāts ir ${square_1(
+  5
+)}`;
+document.getElementById("demo").innerHTML =
+  document.getElementById("demo").innerHTML +
+  `<br>Skaitļa ${7} kvadrāts ir ${square_2(7)}`;
+
+console.log(`Faktoriāls no ${3} ir ${factorial(3)}`); // 3! = 1 * 2 * 3 = 2! * 3 = 1! * 2 * 3   NB! 0! = 1
